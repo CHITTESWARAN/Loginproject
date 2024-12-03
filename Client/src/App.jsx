ba5fe8fdb9899752aaa1bff42ebe5d48ec13c352
CHITTESWARAN <chitteswarancj06@gmail.com>
2024-12-03 11:16:54 +0530

    first commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,6 @@ import Login from './Login';
 import { Routes, Route } from 'react-router-dom'; 
 import { UserContext, UserProvider } from './UserContext';
 import Index from './Index';
-import Display from './Display';
 
 const App = () => {
   return (
@@ -15,7 +14,6 @@ const App = () => {
       <div className='mt-28 text-2xl font-serif text-black'>
       <Routes>
       <Route path="/" element={<Index />} />
-      <Route path="/Display" element={<Display />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
